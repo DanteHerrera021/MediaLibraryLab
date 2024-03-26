e613dfb358cf488b002504bb360e01b1c4ede889
Dante Herrera <dherrera3@my.wctc.edu>
2024-03-25 20:31:03 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+logger.Info(scrubbedFile);
+MovieFile movieFile = new MovieFile(scrubbedFile);
+
+string userInput = "";
+
+do
+{
+
+    Console.WriteLine("Welcome to the Media Library Lab!");
+    Console.WriteLine("Please type 1 to view all available movies");
+    Console.WriteLine("Please type 2 to add a new movie");
+    Console.WriteLine("Press enter to exit the program.");
+
+    userInput = Console.ReadLine();
+
+    if (userInput == "1")
+    {
+        // TODO: ADD VIEW OPTIONS
+    }
+    else if (userInput == "2")
+    {
+        // TODO: ALLOW FOR MOVIE ADDITION
+    }
+
+} while (userInput == "1" || userInput == "2");
+
+logger.Info("Program ended");
